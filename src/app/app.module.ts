01d68e147935c1bc66f287afc91ed43cc10f186f
Balaraju P <Balaraju.P@healthfirsttech.local>
2024-02-17 11:07:01 +0530

    common styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgbModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,55 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { StatusComponent } from './components/status/status.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { AlertNotificationComponent } from './components/alert-notification/alert-notification.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { TruncatePipe } from './pipes/truncate/truncate.pipe';
+import { DateFormatPipe } from './pipes/date-format/date-format.pipe';
+import { HightlightFiltervaluePipe } from './pipes/highlight-filtervalue/hightlight-filtervalue.pipe';
+import { AccessCheckPipe } from './pipes/accessCheck/access-check.pipe';
+import { MaterialModule } from '../materials/material.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoDataFoundComponent } from './components/no-data-found/no-data-found.component';
+import { InformationDetailsComponent } from './components/information-details/information-details.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StatusComponent,
+    NotificationComponent,
+    LoaderComponent,
+    AlertNotificationComponent,
+    ConfirmationComponent,
+    NoDataFoundComponent,
+    InformationDetailsComponent,
+    FileUploadComponent,
+  ],
   imports: [
-    CommonModule
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  exports: [
+    StatusComponent,
+    NotificationComponent,
+    LoaderComponent,
+    AlertNotificationComponent,
+    ConfirmationComponent,
+    NoDataFoundComponent,
+    MaterialModule
+
+  ],
+  providers: [
+    CurrencyPipe,
+    TruncatePipe,
+    HightlightFiltervaluePipe,
+    DateFormatPipe,
+    AccessCheckPipe
   ]
 })
 export class SharedModule { }
